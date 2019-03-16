0be9e823abb9aba8cc1fb37abce2127fb505ec51
luongtruonggiang <48613656+luongtruonggiang@users.noreply.github.com>
2019-03-16 13:52:05 +0700

    commit4

---
+++ Index.php
@@ -0,0 +1,12 @@
+<?php include_once("header.php"); ?>
+  <ul class= "menu">
+    <li>
+      <a href="/Lab3/list_product.php">Danh sách sản phẩm</a>
+    </li>
+    <li>
+      <a href="/Lab3/add_product.php">Thêm sản phẩm</a>
+    </li>
+  </ul>
+  <?php include_once("footer.php"); ?>
+
+    
